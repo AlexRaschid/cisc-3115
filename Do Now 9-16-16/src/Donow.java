343457a3a6c74e456063b3d94a13b1021ac3fa63
Alexander Raschid <alexraschid1337@gmail.com>
2016-09-16 08:39:55 -0400

    Initial Commit

---
+++ Do Now 9-16-16/src/Donow.java
@@ -0,0 +1,15 @@
+public class Donow {
+
+	
+	
+	public static void main(String[] args) {
+		System.out.println("/ \\ // \\\\ /// \\\\\\");
+		System.out.println(" ");
+		System.out.println(" (\\__/)");
+		System.out.println(" (o^.^)");
+		System.out.println("z(_(\")(\")");
+
+		
+		
+	}
+}
