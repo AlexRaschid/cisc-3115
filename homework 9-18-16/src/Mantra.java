1d4a477c7d4c54fcf2d78db6041aab8fc8d5f834
Alexander Raschid <alexraschid1337@gmail.com>
2016-09-18 18:14:54 -0400

    inital commit

---
+++ homework 9-18-16/src/Mantra.java
@@ -0,0 +1,14 @@
+public class Mantra {
+	public static void main(String[] args){
+		print();
+		print();
+	}
+	
+	
+	public static void print(){
+		System.out.println("There's one thing every coder must understand:");
+		System.out.println("The System.out.println command.");
+		System.out.println("");
+	}
+	
+}
---
+++ homework 9-18-16/src/Stewie2.java
@@ -0,0 +1,17 @@
+
+public class Stewie2 {
+
+	public static void victory(){
+		System.out.println("|| Victory is mine! ||");
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+		
+	}
+	public static void main(String[] args) {
+		System.out.println("//////////////////////");
+		for(int i = 0; i < 5; i++){
+			victory();
+		}
+		
+	}
+
+}
