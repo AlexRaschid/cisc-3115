449e07a25939eb8d2e26e4b9d8711c6a3b59f617
Alexander Raschid <alexraschid1337@gmail.com>
2016-09-20 02:27:15 -0400

    Deleted 'Pokeball' and 'Hello world files' and placed them in here

---
+++ Homework 9-15-16/src/pokeball.java
@@ -0,0 +1,32 @@
+
+public class pokeball {
+
+	/**
+	 * @param args
+	 */
+	
+	//Question 1:
+	
+	public static void main(String[] args) {
+		System.out.println("   @@@@@@@@@@");
+		System.out.println("  @@@@@@@@@@@@");
+		System.out.println(" @@@@@@@@@@@@@@");
+		System.out.println("@@@@@@@@@@@@@@@@");
+		System.out.println("|||||[\"\"\"\"]|||||");
+		System.out.println("|||||[....]|||||");
+		System.out.println("@@@@@@@@@@@@@@@@");
+		System.out.println(" @@@@@@@@@@@@@@ ");
+		System.out.println("  @@@@@@@@@@@@");
+		System.out.println("   @@@@@@@@@@   ");
+	}
+	
+	//Question two:
+	
+	/*This Pokemon graphic can be used in the opening sequence of my Pokemon game,
+	 * or any time the player needs to catch a wild Pokemon.*/
+	
+	
+	//This program was made for a AP computer science asignment.
+	
+	
+}
