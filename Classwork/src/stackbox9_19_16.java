cf2a383476738a6d666a0283da6c684035dbfb6a
Alexander Raschid <alexraschid1337@gmail.com>
2016-09-20 02:28:23 -0400

    Initial Commit

---
+++ Classwork/src/stackbox9_19_16.java
@@ -0,0 +1,16 @@
+public class stackbox9_19_16 {
+	public static void box(){
+		System.out.println("X----X");
+		System.out.println("|    |");
+		System.out.println("|    |");
+		System.out.println("X----X\n");
+	}
+	
+	
+	public static void main(String[] args) {
+		for(int i = 0; i < 3; i++){
+			box();
+		}
+	}
+
+}
