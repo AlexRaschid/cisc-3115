9e431044a3401130cb3da2549d4cbe8b9aa50c7f
Alexander Raschid <alexraschid1337@gmail.com>
2016-10-16 17:16:26 -0400

    Updated homework, did not originaly follow requirements.

--- Homework 10-11-16/src/drawThis22.java
+++ Homework 10-11-16/src/drawThis22.java
@@ -2,15 +2,36 @@
 public class drawThis22 {
 	
 	 public static void drawThis22() {
-		 for (int i = 1; i <= 10; i++) {
-		 for (int j = 1; j <= (10 - i); j++) {
-		 System.out.print(" ");
-		 }
-		 for (int j = 1; j <= 2 * i - 1; j++) {
-		 System.out.print("*");
-		 }
+		 for (int i = 1; i <= 8; i++) {
+			 for (int j = 1; j <= (10 - i); j++) {
+				 	System.out.print("\\");
+			 }
+			 for (int j = 1; j <= 2 * i - 1; j++) {
+				 	System.out.print("!");
+			 }
+			 for (int j = 1; j <= (10 - i); j++) {
+				 	System.out.print("/");
+			 }
 		 System.out.println();
 		 }
+		 //The following shape is the inverse of the one made above.
+		 
+		 System.out.println();
+		 
+		 for (int i = 8; i >= 1; i--) {
+			 for (int j = 1; j <= (10 - 2 - i); j++) {
+				 	System.out.print("\\");
+			 }
+			 for (int j = 1; j <= 2 * i; j++  ) {
+				 	System.out.print("!");
+			 }
+			 for (int j = 1; j <= (10 - 2 - i); j++) {
+				 	System.out.print("/");
+			 }
+		 System.out.println();
+		 }
+		 
+		 
 	} 
 	
 	
