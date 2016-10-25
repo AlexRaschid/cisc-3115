003eda77a6417768e484bc4396b82ba46baa86a4
Alexander Raschid <alexraschid1337@gmail.com>
2016-10-24 21:09:11 -0400

    Updated Some donows, added new homework.

---
+++ Do Nows/src/donow10_18_16.java
@@ -0,0 +1,15 @@
+
+public class donow10_18_16 {
+
+	public static void main(String[] args) {
+		int j;
+		int k;
+		for(j = 1; j <= 5; j++){	
+			for(k = 1; k <= 1 * j; k++){
+				System.out.print("\"");
+			}
+			System.out.println("");
+		}
+
+	}
+}
