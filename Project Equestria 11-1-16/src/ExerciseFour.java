f5bf13cd56ae5de8eacc07be07b0836e4f7d40a8
Alexander Raschid <alexraschid1337@gmail.com>
2016-11-05 18:16:17 -0400

    Initial

---
+++ Project Equestria 11-1-16/src/ExerciseFour.java
@@ -0,0 +1,13 @@
+public class ExerciseFour {
+	public static void longestTrip(int x1, int y1,//First location
+								   int x2, int y2,//Second location
+								   int x3, int y3){//Third Location
+		
+	
+	}
+	
+	public static void main(String[] args){
+		
+		
+    }
+}
--- Project Equestria 11-1-16/src/ExerciseOne.java
+++ Project Equestria 11-1-16/src/ExerciseOne.java
@@ -1,7 +1,7 @@
 
 public class ExerciseOne {
 	  public static void distance(int x1, int y1, int x2, int y2){
-	  		double distance = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+	  		double distance = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 	   		System.out.print("The distance between the points (" + x1 + "," + y1 + ") and (" + x2 + "," + y2 + ") is = " + distance);
 	  
 	  }
