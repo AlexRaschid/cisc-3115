9858c3540dd17797d7a19401fd3bd183be64ac5c
Alexander Raschid <alexraschid1337@gmail.com>
2016-11-06 22:56:34 -0500

    Completed Exercise four and three

--- Project Equestria 11-1-16/src/ExerciseThree.java
+++ Project Equestria 11-1-16/src/ExerciseThree.java
@@ -4,17 +4,18 @@ public class ExerciseThree {
 								   int x3, int y3){//Third Location
 
 
-				/* double distanceOneTwo = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+				double distanceOneTwo = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 				double distanceOneThree = Math.sqrt((x3-x1)*(x3-x1) + (y3-y1)*(y3-y1));
 				double distanceTwoThree = Math.sqrt((x3-x2)*(x3-x2) + (y3-y2)*(y3-y2));
 				
 				
-				double /*Two is vertex angleOneTwoThree = acos(() / ());
-				double /*Three is vertex angleTwoThreeOne = acos( () / ());
-				double /*One is vertex angleThreeOneTwo = acos(  ((distanceOneTwo * distanceOneTwo) + (distanceOneThree * distanceOneThree) - (distanceTwoThree * distanceTwoThree)) / (2 * distanceOneTwo * distanceOneThree));
+				double /*Two is vertex*/ angleOneTwoThree = Math.acos( ((distanceOneTwo * distanceOneTwo) - (distanceOneThree * distanceOneThree) - (distanceTwoThree * distanceTwoThree)) / (2 * distanceOneTwo * distanceOneThree) );
+				double /*Three is vertex*/angleTwoThreeOne = Math.acos( ((distanceOneTwo * distanceOneTwo) - (distanceOneThree * distanceOneThree) - (distanceTwoThree * distanceTwoThree)) / (2 * distanceOneTwo * distanceOneThree) );
+				double /*One is vertex*/ angleThreeOneTwo = Math.acos( ((distanceOneTwo * distanceOneTwo) - (distanceOneThree * distanceOneThree) - (distanceTwoThree * distanceTwoThree)) / (2 * distanceOneTwo * distanceOneThree));
 		
-				System.out.println("From Point One, turn " +  + " degreese and walk " + + " to reach Point Two");
-*/
+				System.out.println("From Point One, turn " + angleThreeOneTwo + " degreese and walk " + distanceOneTwo + " to reach Point Two.");
+				System.out.println("From Point Two, turn " + angleOneTwoThree + " degreese to your right and walk " + distanceTwoThree + " miles to reach point Three.");
+				System.out.println("To return to Point One from Point three, turn " + angleTwoThreeOne + " degreese to your right and walk " + distanceOneThree + " miles.");
 	}
 
 	public static void main(String[] args){
